cad850e71554e893bced283c744f6d2937d2f3e9
Eric Jaslet <eric.jaslet@ericjaslet.fr>
2021-11-30 16:02:00 +0100

    Lesson modification field

--- src/Controller/Admin/LessonCrudController.php
+++ src/Controller/Admin/LessonCrudController.php
@@ -5,6 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Lesson;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+
 class LessonCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
@@ -12,14 +16,13 @@ class LessonCrudController extends AbstractCrudController
         return Lesson::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextEditorField::new('content'),
         ];
     }
-    */
+    
 }
