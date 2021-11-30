40fbb0c684f96d131b371d8f690ff0d43fd39971
CamileGhastine <camileghastine@hotmail.com>
2021-11-30 22:03:08 +0100

    lesson by category

--- src/Controller/Front/FrontController.php
+++ src/Controller/Front/FrontController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Front;
 
 use App\Entity\Lesson;
+use App\Entity\Category;
 use App\Repository\LessonRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,25 +13,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class FrontController extends AbstractController
 {
     private LessonRepository $lessonRepository;
+    private CategoryRepository $categoryRepository;
 
-    public function __construct(LessonRepository $lessonRepository)
+    public function __construct(LessonRepository $lessonRepository, CategoryRepository $categoryRepository)
     {
         $this->lessonRepository = $lessonRepository;
+        $this->categoryRepository = $categoryRepository;
     }
 
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function home(): Response
     {
         return $this->render('front/index.html.twig', [
-            'lessons' => $this->lessonRepository->findall()
+            'lessons' => $this->lessonRepository->findall(),
+            'categories' => $this->categoryRepository->findAll()
         ]);
     }
 
-    #[Route('/lesson/{id<[0-9]+>}', name: 'show')]
+    #[Route('/lesson', name: 'all_lessons')]
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'lessons' => $this->lessonRepository->findall(),
+            'categories' => $this->categoryRepository->findAll()
+        ]);
+    }
+
+    #[Route('/lesson/category/{id<[0-9]+>}', name: 'lessons_by_category')]
+    public function indexByCategory(Category $category): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'lessons' => $this->lessonRepository->findBy(['category' => $category]),
+            'categories' => $this->categoryRepository->findAll()
+        ]);
+    }
+
+    #[Route('/lesson/{id<[0-9]+>}', name: 'show_lesson')]
     public function show(Lesson $lesson)
     {
         return $this->render('front/show.html.twig', [
             'lesson' => $lesson,
+            'categories' => $this->categoryRepository->findAll()
         ]);
     }
 }
