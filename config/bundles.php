5ada5969633504f6420f26a05343779bdd8b3615
CamileGhastine <camileghastine@hotmail.com>
2021-11-30 21:29:21 +0100

    show lesson

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,7 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/Front/FrontController.php
+++ src/Controller/Front/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\Lesson;
 use App\Repository\LessonRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +10,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FrontController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index(LessonRepository $lessonRepository): Response
-    {
-        $lessons = $lessonRepository->findall();
+    private LessonRepository $lessonRepository;
 
+    public function __construct(LessonRepository $lessonRepository)
+    {
+        $this->lessonRepository = $lessonRepository;
+    }
+
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
         return $this->render('front/index.html.twig', [
-            'lessons' => $lessons
+            'lessons' => $this->lessonRepository->findall()
+        ]);
+    }
+
+    #[Route('/lesson/{id<[0-9]+>}', name: 'show')]
+    public function show(Lesson $lesson)
+    {
+        return $this->render('front/show.html.twig', [
+            'lesson' => $lesson,
         ]);
     }
 }
