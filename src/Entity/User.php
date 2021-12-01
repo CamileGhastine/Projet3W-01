d947e2ebc8cd2fa55f2cb5579dd54f897087bace
nicolaszanforlini <you@example.com>
2021-12-01 13:53:23 +0100

    tu vas MARCHERRRR

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,7 +25,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      * @Assert\Email(
-     *     message = "Votre email '{{ value }}' n'est pas valide."
+     *     message = "Votre email '{{ value }}' n'est pas valide. "
      * )
      */
     private $email;
