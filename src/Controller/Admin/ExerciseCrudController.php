136dbcc6a93b7a6e8c150b8e58b0f69ae6b9aee9
Eric Jaslet <eric.jaslet@ericjaslet.fr>
2021-12-02 18:21:14 +0100

    slice twig and add variable twig

--- src/Controller/Admin/ExerciseCrudController.php
+++ src/Controller/Admin/ExerciseCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Exercise;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class ExerciseCrudController extends AbstractCrudController
 {
@@ -12,14 +15,11 @@ class ExerciseCrudController extends AbstractCrudController
         return Exercise::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextareaField::new('content', 'Contenu')->hideOnIndex(),
         ];
     }
-    */
 }
